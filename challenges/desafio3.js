3ae0bef8ec219e8bf3e7f07664391eaf272852f0
RafaMaths <rmathscoach@gmail.com>
2021-08-19 21:39:51 -0300

    challenge 3: DONE

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -1,4 +1,24 @@
-// use ('aggregations');
-// db.movies.find();
-
-// use ('aggregations');
+db.movies.aggregate([
+  {
+    $match: {
+      $and: [
+        { "imdb.rating": { $gte: 7 } },
+        { $nor: [{ genres: "Crime" }, { genres: "Horror" }] },
+        { rated: { $in: ["PG", "G"] } },
+        { languages: { $all: ["English", "Spanish"] } },
+      ],
+    },
+  },
+  {
+    $project: {
+      titulo: "$title",
+      avaliado: "$rated",
+      notaIMDB: "$imdb.rating",
+      votosIMDB: "$imdb.votes",
+      ano: "$year",
+      _id: 0,
+    },
+  }, {
+    $sort: { ano: -1, notaIMDB: -1, titulo: 1 },
+  },
+]);
