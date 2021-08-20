bcca0f923214db0e6431a1e6e4c451256338a3f4
RafaMaths <rmathscoach@gmail.com>
2021-08-20 15:34:03 -0300

    challenge 5: development

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,4 +1,35 @@
 // use("aggregations");
 // db.movies.find();
+// const favActors = ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"]
 
 // use("aggregations");
+// db.movies.aggregate([{
+//   $match: {
+//     cast: {
+//       $in: ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"]
+//       },
+//     countries: "USA",
+//     "tomatoes.viewer.rating": {$gte: 3}
+//     }
+//   }, {
+//     $addFields: {
+//       num_favs: {
+//         $count: 'string'
+//       }
+//     }
+//   }
+// ]);
+
+// use("aggregations");
+// db.movies.aggregate([
+//   {
+//     $match: {
+//       $and: [
+//         { "imdb.rating": { $gte: 7 } },
+//         { $nor: [{ genres: "Crime" }, { genres: "Horror" }] },
+//         { rated: { $in: ["PG", "G"] } },
+//         { languages: { $all: ["English", "Spanish"] } },
+//       ],
+//     },
+//   },
+// ]);
