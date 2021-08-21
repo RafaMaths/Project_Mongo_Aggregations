c6eb4123ba7be111c2aa91ef46838fd97339731a
RafaMaths <rmathscoach@gmail.com>
2021-08-21 16:00:40 -0300

    challenge 8: done

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -1,4 +1,33 @@
-// use("aggregations");
-// db.movies.find();
-
-// use("aggregations");
+db.air_alliances.aggregate([{
+  $unwind: "$airlines",
+}, {
+  $lookup: {
+    from: "air_routes",
+    let: { airlineName: "$airlines" },
+    pipeline: [
+      { $match: {
+        $expr: {
+          $eq: ["$$airlineName", "$airline.name"],
+        },
+      } },
+    ],
+    as: "routesForGroup",
+  },
+}, {
+  $unwind: "$routesForGroup",
+}, {
+  $match: {
+    name: "SkyTeam",
+    "routesForGroup.airplane": { $in: ["747", "380"] },
+  },
+}, {
+  $group: {
+    _id: "$name",
+    somaRotas: { $sum: 1 },
+  },
+}, {
+  $project: {
+    totalRotas: { $arrayToObject: "$somaRotas" },
+  },
+},
+]);
