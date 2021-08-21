33f8394b37dfbb98181068830b3758144aabc744
RafaMaths <rmathscoach@gmail.com>
2021-08-21 14:05:58 -0300

    challenge 7: DONE

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -1,4 +1,20 @@
-// use("aggregations");
-// db.movies.find();
-
-// use("aggregations");
+db.movies.aggregate([{
+  $match: {
+    languages: { $all: ["English"] },
+    cast: { $exists: 1 },
+  },
+}, {
+  $unwind: "$cast",
+}, {
+  $group: {
+    _id: "$cast",
+    numeroFilmes: { $sum: 1 },
+    avgIMDB: { $avg: "$imdb.rating" },
+  },
+}, {
+  $sort: { numeroFilmes: -1, cast: -1 } }, {
+  $project: {
+    numeroFilmes: 1,
+    mediaIMDB: { $round: ["$avgIMDB", 1] },
+  },
+}]);
