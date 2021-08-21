3fa692664e364827770383a067e90c104318290c
RafaMaths <rmathscoach@gmail.com>
2021-08-20 21:54:36 -0300

    challenge 6: DONE

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,4 +1,26 @@
-// use("aggregations");
-// db.movies.find();
+db.movies.aggregate([{
+  $match: {
+    awards: {
+      $regex: /(won\s.\soscar)|(won\s.\sorcars)/gi,
+    },
+  },
+},
+{
+  $group: {
+    _id: null,
+    maior_rating: { $max: "$imdb.rating" },
+    menor_rating: { $min: "$imdb.rating" },
+    avg_rating: { $avg: "$imdb.rating" },
+    devSamp: { $stdDevSamp: "$imdb.rating" } },
 
-// use("aggregations");
+},
+{
+
+  $project: {
+    _id: 0,
+    maior_rating: 1,
+    menor_rating: 1,
+    media_rating: { $round: ["$avg_rating", 1] },
+    desvio_padrao: { $round: ["$devSamp", 1] },
+  },
+}]);
