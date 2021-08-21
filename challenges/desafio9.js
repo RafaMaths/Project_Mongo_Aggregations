78fb533df9f0ae80a4b7d78458c38fd64f1244ba
RafaMaths <rmathscoach@gmail.com>
2021-08-21 20:12:02 -0300

    challenge 9: DONE

--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -1,4 +1,20 @@
-// use("aggregations");
-// db.movies.find();
-
-// use("aggregations");
+db.trips.aggregate([{
+  $match: {
+    birthYear: { $exists: 1, $ne: "" },
+  },
+}, {
+  $addFields: {
+    minBornToInt: { $toInt: "$birthYear" },
+    maxBornToInt: { $toInt: "$birthYear" },
+  } }, {
+  $group: {
+    _id: null,
+    maiorAnoNascimento: { $max: "$maxBornToInt" },
+    menorAnoNascimento: { $min: "$minBornToInt" },
+  } }, {
+  $project: {
+    _id: 0,
+    maiorAnoNascimento: 1,
+    menorAnoNascimento: 1,
+  },
+}]);
