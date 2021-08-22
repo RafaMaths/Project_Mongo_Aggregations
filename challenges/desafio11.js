380e460c220590203baa97e5349d5636a60607d9
RafaMaths <rmathscoach@gmail.com>
2021-08-22 19:12:37 -0300

    challenge 11: DONE

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -1,4 +1,20 @@
-// use("aggregations");
-// db.movies.find();
-
-// use("aggregations");
+db.trips.aggregate([{
+  $addFields: {
+    dayOfWeek: { $dayOfWeek: "$startTime" },
+  },
+}, {
+  $group: {
+    _id: "$dayOfWeek",
+    totalDays: { $count: {} },
+  },
+}, {
+  $sort: { totalDays: -1 },
+}, {
+  $limit: 1,
+}, {
+  $project: {
+    _id: 0,
+    diaDaSemana: "$_id",
+    total: "$totalDays",
+  },
+}]);
