fa76970e42d2fd9a12012ed4f5aa626f943c50bf
RafaMaths <rmathscoach@gmail.com>
2021-08-22 20:38:30 -0300

    challenge 13: DONE

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,4 +1,31 @@
-// use("aggregations");
-// db.movies.find();
+db.trips.aggregate([{
+  $match: {
+    startTime: { $gt: new Date("2016-03-09T23:59:59Z"), $lt: new Date("2016-03-11T00:00:00Z") },
+  },
+},
+{
+  $addFields: {
+    day: {
+      $dayOfWeek: "$startTime",
+    },
+    timeDiference: {
+      $subtract: ["$stopTime", "$startTime"],
+    },
+  },
+},
+{
+  $group: {
+    _id: "$day",
+    avgTime: { $avg: "$timeDiference" },
 
-// use("aggregations");
+  },
+}, {
+  $addFields: {
+    timeToHour: { $divide: ["$avgTime", 1000 * 60] },
+  },
+}, {
+  $project: {
+    _id: 0,
+    duracaoMediaemMinutos: { $ceil: "$timeToHour" },
+  },
+}]);
