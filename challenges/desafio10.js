cee0e4f86a8293f7b2a0976b34f9fe7c6a66695b
RafaMaths <rmathscoach@gmail.com>
2021-08-22 15:27:10 -0300

    challenge 10: DONE

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,4 +1,27 @@
-// use("aggregations");
-// db.movies.find();
+db.trips.aggregate([{
+  $addFields: {
+    timeDiference: {
+      $subtract: ["$stopTime", "$startTime"],
+    },
+  },
+}, {
+  $group: {
+    _id: "$usertype",
+    avgTravel: { $avg: "$timeDiference" },
+  },
+}, {
+  $addFields: {
+    travelHours: { $divide: ["$avgTravel", 1000 * 60 * 60] },
 
-// use("aggregations");
+  },
+}, {
+  $sort: { travelHours: 1 },
+
+}, {
+  $project: {
+    _id: 0,
+    tipo: "$_id",
+    duracaoMedia: { $round: ["$travelHours", 2] },
+  },
+},
+]);
