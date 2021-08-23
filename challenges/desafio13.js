a9171324bd98c6a20d2fd6c6d380603b09de51a1
RafaMaths <rmathscoach@gmail.com>
2021-08-22 21:42:24 -0300

    challente 7 and 13: refatored

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,31 +1,20 @@
-db.trips.aggregate([{
-  $match: {
-    startTime: { $gt: new Date("2016-03-09T23:59:59Z"), $lt: new Date("2016-03-11T00:00:00Z") },
-  },
-},
-{
-  $addFields: {
-    day: {
-      $dayOfWeek: "$startTime",
-    },
-    timeDiference: {
-      $subtract: ["$stopTime", "$startTime"],
+use("aggregations");
+db.trips.aggregate([
+  {
+    $match: {
+      startTime: { $gt: new Date("2016-03-09T23:59:59Z"), $lt: new Date("2016-03-11T00:00:00Z") },
     },
   },
-},
-{
-  $group: {
-    _id: "$day",
-    avgTime: { $avg: "$timeDiference" },
-
+  {
+    $group: {
+      _id: null,
+      avgTime: { $avg: { $subtract: ["$stopTime", "$startTime"] } },
+    },
   },
-}, {
-  $addFields: {
-    timeToMinute: { $divide: ["$avgTime", 1000 * 60] },
+  {
+    $project: {
+      _id: 0,
+      duracaoMediaemMinutos: { $ceil: { $divide: ["$avgTime", 1000 * 60] } },
+    },
   },
-}, {
-  $project: {
-    _id: 0,
-    duracaoMediaemMinutos: { $ceil: "$timeToMinute" },
-  },
-}]);
+]);
--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -1,22 +1,28 @@
-db.movies.aggregate([{
-  $match: {
-    languages: { $all: ["English"] },
-    cast: { $exists: true },
+db.movies.aggregate([
+  {
+    $match: {
+      languages: { $all: ["English"] },
+      cast: { $exists: true },
+    },
   },
-}, {
-  $unwind: "$cast",
-}, {
-  $group: {
-    _id: "$cast",
-    numeroFilmes: { $count: {} },
-    avgIMDB: { $avg: "$imdb.rating" },
+  {
+    $unwind: "$cast",
   },
-}, {
-  $sort: { numeroFilmes: -1, _id: -1 },
-}, {
-  $project: {
-    _id: 1,
-    numeroFilmes: 1,
-    mediaIMDB: { $round: ["$avgIMDB", 1] },
+  {
+    $group: {
+      _id: "$cast",
+      numeroFilmes: { $count: {} },
+      avgIMDB: { $avg: "$imdb.rating" },
+    },
   },
-}]);
+  {
+    $project: {
+      _id: 1,
+      numeroFilmes: 1,
+      mediaIMDB: { $round: ["$avgIMDB", 1] },
+    },
+  },
+  {
+    $sort: { numeroFilmes: -1, _id: -1 },
+  },
+]);
